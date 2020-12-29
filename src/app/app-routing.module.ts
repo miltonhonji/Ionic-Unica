906b7d2ad47325ac17abee1738b963dbba79d097
Milton Honji <factorysolutionIT@outlook.com>
2020-12-29 12:47:12 -0300

    Ionic - Carreira Unica

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'carreira-unica',
+    loadChildren: () => import('./carreira-unica/carreira-unica.module').then( m => m.CarreiraUnicaPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
